f0e88f3f596791f7befc240c5d2c3a1790f45b9e
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-03-31 21:39:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
- const promises = [
-    new Promise(resolve => setTimeout(() => resolve(Math.random() * 2 + 1), Math.random() * 2000 + 1000)),
-    new Promise(resolve => setTimeout(() => resolve(Math.random() * 2 + 1), Math.random() * 2000 + 1000)),
-    new Promise(resolve => setTimeout(() => resolve(Math.random() * 2 + 1), Math.random() * 2000 + 1000))
-  ];
+//your JS code here. If required.
+// create three promises that resolve after a random time between 1 and 3 seconds
+  const promise1 = new Promise((resolve) => setTimeout(() => resolve('Promise 1'), Math.random() * 2000 + 1000));
+  const promise2 = new Promise((resolve) => setTimeout(() => resolve('Promise 2'), Math.random() * 2000 + 1000));
+  const promise3 = new Promise((resolve) => setTimeout(() => resolve('Promise 3'), Math.random() * 2000 + 1000));
 
-  Promise.all(promises)
-    .then(results => {
-      const loadingRow = document.getElementById("loading");
+  // wait for all promises to resolve using Promise.all
+  Promise.all([promise1, promise2, promise3])
+    .then((results) => {
+      // remove the loading text
+      const loadingRow = document.getElementById('loading');
       loadingRow.parentNode.removeChild(loadingRow);
 
-      const table = document.querySelector("table");
-      let total = 0;
-
+      // populate the table with the results
+      const tableBody = document.querySelector('tbody');
       results.forEach((result, index) => {
-        const promiseNum = index + 1;
-        const timeTaken = result.toFixed(3);
-        total += result;
-        const row = table.insertRow();
+        const row = tableBody.insertRow(index);
         const promiseCell = row.insertCell(0);
+        promiseCell.textContent = result;
         const timeTakenCell = row.insertCell(1);
-        promiseCell.textContent = `Promise ${promiseNum}`;
-        timeTakenCell.textContent = timeTaken;
+        timeTakenCell.textContent = ((index + 1) * 1000 + Math.random() * 2000) / 1000;
       });
 
-      const totalRow = table.insertRow();
+      // add a row for the total time taken
+      const totalRow = tableBody.insertRow();
       const totalCell = totalRow.insertCell(0);
-      const totalTimeCell = totalRow.insertCell(1);
-      totalCell.textContent = "Total";
-      totalTimeCell.textContent = total.toFixed(3);
+      totalCell.textContent = 'Total';
+      const totalTimeTakenCell = totalRow.insertCell(1);
+      totalTimeTakenCell.textContent = (Date.now() - performance.timing.navigationStart) / 1000;
     });
+       
